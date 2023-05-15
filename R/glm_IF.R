acf024fe514bcf62d34fc3ae0503951586422d88
Li Ge 葛立 <li.ge@outlook.com>
2023-05-15 18:36:59 -0500

    `glm_IF()` supports `mgcv::gam()` obj

--- R/glm_IF.R
+++ R/glm_IF.R
@@ -1,4 +1,4 @@
-#' Empirical influence function of glm object
+#' Empirical influence function of glm/gam object
 #'
 #' @param glm_obj The object returned by glm().
 #'
@@ -11,10 +11,14 @@
 #' y <- rbinom(n = 1e3, size = 1, plogis(-1 + x1 + 2 * x2))
 #' glm_obj <- glm(y ~ x1 + x2, family = binomial(link = "logit"))
 #' all.equal(crossprod(glm_IF(glm_obj)) / 1e6, sandwich::vcovHC(glm_obj, type = "HC"))
+#' # Also works with mgcv::gam object for nonparametric first-step estimation
+#' library(mgcv)
+#' gam_obj <- gam(y ~ s(x1) + s(x2), family = binomial(link = "logit"), method = "REML")
+#' all.equal(crossprod(glm_IF(gam_obj)) / 1e6, sandwich::vcovHC(gam_obj, type = "HC"))
 glm_IF <- function(glm_obj) {
   # https://github.com/ohines/plmed/issues/1
   # Thanks to Oliver Hines.
   # This is slightly slower but won't affect performance much
   # since it's still O(n).
-  sandwich::estfun(glm_obj)%*%sandwich::bread(glm_obj)
+  sandwich::estfun(glm_obj) %*% sandwich::bread(glm_obj)
 }
